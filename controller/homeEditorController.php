5f61c308919bd3b9177ed24a265cb9480763cb32
MeliAbi <melina.abillera@gmail.com>
2023-11-15 00:55:34 -0300

    Version 3.4.0

--- controller/homeEditorController.php
+++ controller/homeEditorController.php
@@ -14,10 +14,50 @@ class homeEditorController
     public function execute()
     {
         if (isset($_SESSION['correo'])&&(isset($_SESSION['rolActual']))&&$_SESSION['rolActual']==2){
-            $this->renderizado->render('/homeEditor');
+            /*PREGUNTAS: (home)
+             * DAR DE ALTA
+             * DAR DE BAJA
+             * MODIFICAR
+             * PREGUNTAS REPORTADAS:
+             * APROBAR
+             * DAR DE BAJA
+             * PREGUNTAS SUGERIDAS:
+             * APROBAR
+             * RECHAZAR
+             */
+            $registros =$this->modelo->tabla();
+            $categorias =$this->modelo->traerCategorias();
+            $arrayPreguntas =$this->modelo->traerPreguntas();
+
+            $data = array(
+                'arrayPreguntas' => $arrayPreguntas,
+            );
+            $this->renderizado->render('/homeEditor', $data);
         }
         else{
              $this->renderizado->render('/login');
         }
-}
+    }
+
+    PUBLIC FUNCTION mostrarTabla(){
+        $registros =$this->modelo->tabla();
+        $html='';
+
+        if(!empty($registros)){
+            while($row = $registros){
+                $html .= '<tr>';
+                $html .= '<td>'. $row['id'] .'</td>';
+                $html .= '<td>'. $row['categoria'] .'</td>';
+                $html .= '<td>'. $row['pregunta'] .'</td>';
+                $html .= '<td><a href="">Editar</a></td>';
+                $html .= '<td><a href="">Eliminar</a></td>';
+                $html .= '</tr>';
+            }
+        }else{
+            $html .= '<tr>';
+            $html .= '<td colspan="7">Sin Resultados</td>';
+            $html .= '</tr>';
+        }
+        echo json_encode($html, JSON_UNESCAPED_UNICODE);
+    }
 }
